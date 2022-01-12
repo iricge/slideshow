bc05ff3e65732c886023281128e672a282ecce20
majid <majidhatamimalekii@gmail.com>
2022-01-12 23:52:47 +0330

    [css] chnaged slides elements from container to each div

--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,4 @@ let slider = document.querySelector(".slider");
 let sliderContainer = document.querySelector(".slider-container");
 let nextBtn = document.querySelector(".next-btn");
 let preBtn = document.querySelector(".pre-btn");
+// commands
