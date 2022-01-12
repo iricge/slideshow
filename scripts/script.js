a3a605d4a948e72fa9b04a0d1fc568439494e46f
majid <majidhatamimalekii@gmail.com>
2022-01-12 12:39:25 +0330

    [js] getting elements

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,5 @@
-var 
+// get elements
+let slider = document.querySelector(".slider");
+let sliderContainer = document.querySelector(".slider-container");
+let nextBtn = document.querySelector(".next-btn");
+let preBtn = document.querySelector(".pre-btn");
